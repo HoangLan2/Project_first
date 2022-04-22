5ad25962f3bd5a7e0b6a8c4a78e6e71f98176f29
HoangLan2 <hoanglan222000@gmail.com>
2022-04-22 11:23:28 +0700

    Commit lan dau

---
+++ Keyist-Ecommerce/client/src/config/local.ts
@@ -0,0 +1,27 @@
+import { Configuration } from './model';
+
+
+export const config: Configuration = {
+  apiUrl: 'http://localhost:8080',
+  authUrl: 'http://localhost:8081',
+  clientId: 'test',
+  clientSecret: 'test',
+  carausel: [
+    {
+      title: 'BÙNG NỔ MÙA HÈ',
+      text: 'Thời trang nâng tầm giá trị',
+      imageUrl: 'https://mcdn.nhanh.vn/store/4902/bn/sb_1602056977_630.jpg'
+    },
+    {
+      title: 'Title',
+      text: 'Text',
+      imageUrl: 'https://mcdn.nhanh.vn/store/4902/bn/sb_1582171458_459.jpg'
+    },
+    {
+      title: 'Title',
+      text: 'Text',
+      imageUrl: 'https://mcdn.nhanh.vn/store/4902/bn/sb_1582171438_43.jpg'
+    }
+  ],
+  bannerUrl: 'https://scontent-hkt1-1.xx.fbcdn.net/v/t39.30808-6/276320400_2104857209685115_2369584396953274877_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=e3f864&_nc_ohc=xE-er2GjtHQAX_QYyoR&_nc_ht=scontent-hkt1-1.xx&oh=00_AT_DyLb2hzABG4S13vJznAd2YPsLgITsiHcnUjHUlW4Ffw&oe=625ED57D'
+};
